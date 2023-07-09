052cdcdb34b327cff0f8e10bd7c20384628af917
Nathan Bridgewater <nathan.bridgewater@accenture.com>
2023-07-09 15:38:31 +0100

    Initial commit

---
+++ source/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import type { Express } from 'express'
+
+const PORT = 8080
+const app: Express = express()
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
