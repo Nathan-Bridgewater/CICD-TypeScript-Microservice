44d4a0aae054993e25d6e4f325c146d0be6b1db9
Nathan Bridgewater <nathanbridgey99@gmail.com>
2023-07-21 14:27:50 +0100

    Flattened file structure and started writing unit tests

--- source/index.ts
+++ source/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
-import type { Express } from 'express'
+import type { Express, Request, Response } from 'express'
 
 const PORT = 8080
 const app: Express = express()
+
+export const helloWorldHandler = (req: Request, res: Response): void => {
+}
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
