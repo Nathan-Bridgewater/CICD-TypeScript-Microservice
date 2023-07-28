865d39f1dd02810f5cdbaac67318fa417f5daf6f
Nathan Bridgewater <nathanbridgey99@gmail.com>
2023-07-28 17:37:52 +0100

    Removed files as they are no longer required

--- source/index.ts
+++ source/index.ts
@@ -4,9 +4,6 @@ import type { Express, Request, Response } from 'express'
 const PORT = 8080
 const app: Express = express()
 
-export const helloWorldHandler = (req: Request, res: Response): void => {
-}
-
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
 })
