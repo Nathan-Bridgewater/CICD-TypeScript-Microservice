a64998ee45510c09ad7ac1d5944d535fcbd20b78
Nathan-Bridgewater <nathanbridgey99@gmail.com>
2023-11-04 17:22:14 +0000

    Updated CDK stack to include basic template for a load balanced fargate service

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,29 @@
 import * as cdk from 'aws-cdk-lib'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as ecs from 'aws-cdk-lib/aws-ecs'
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns'
 import { type Construct } from 'constructs'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor (scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 3
+    })
+
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc:vpc
+    })
+
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateServce', {
+      cluster: cluster,
+      cpu: 512,
+      desiredCount: 6,
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample') },
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true
+    })
+
   }
 }
